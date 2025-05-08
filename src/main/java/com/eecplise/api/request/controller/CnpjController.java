51111c9e4f9c2fdd78bf671b78de7fe20c03fa83
eeclipse <gabriel@teste.com>
2025-05-08 16:51:05 -0300

    criação_controller_para_resttemplate

---
+++ src/main/java/com/eecplise/api/request/controller/CnpjController.java
@@ -0,0 +1,20 @@
+package com.eecplise.api.request.controller;
+
+import com.eecplise.api.request.dto.CnpjResponseDTO;
+import com.eecplise.api.request.service.CnpjService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cnpj")
+public class CnpjController {
+
+    @Autowired
+    private CnpjService cnpjService;
+
+
+    @GetMapping("/{cnpj}")
+    public CnpjResponseDTO consultarCnpj(@PathVariable String cnpj) {
+        return cnpjService.consultarCnpj(cnpj);
+    }
+}
--- src/main/java/com/eecplise/api/request/service/CnpjService.java
+++ src/main/java/com/eecplise/api/request/service/CnpjService.java
@@ -1,21 +1,27 @@
 package com.eecplise.api.request.service;
 
 import com.eecplise.api.request.dto.CnpjResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-
 public class CnpjService {
 
     @Value("${api.invertexto.token}")
-    private  String token;
+    private String token;
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public CnpjService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public CnpjResponseDTO consultarCnpj(String cnpj) {
         String url = "https://api.invertexto.com/v1/cnpj/" + cnpj + "?token=" + token;
         return restTemplate.getForObject(url, CnpjResponseDTO.class);
     }
 }
+
